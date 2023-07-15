6717adf64ec9df3fa7a1b4d17d7b2aac054635e3
avs920x <128978289+avs920x@users.noreply.github.com>
2023-07-15 14:35:04 -0400

    Create quotescript.js

---
+++ quotescript.js
@@ -0,0 +1,42 @@
+//define constant variables
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+let apiQuotes = [];
+
+
+//show new quote
+function newQuote() {
+    //pick random quote from api array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
+    if(!quote.author) {
+        authorText.textContent = "Unknown";
+    } else {
+        authorText.textContent = quote.author;
+    }
+}
+
+
+//get quotes via API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        //catch error here
+    }
+}
+
+
+//on load
+getQuotes();
+
+
